3bcba9cde21f05299da7496990b61d3c6bfd029a
Silver1078682 <ShinySmoothSilver@outlook.com>
2026-02-28 13:02:53 +0800

    Chore

--- addons/gdlib/utils/rand.gd
+++ addons/gdlib/utils/rand.gd
@@ -33,6 +33,7 @@ static func element(obj: Variant) -> Variant:
 static func between(a: Variant, b: Variant) -> Variant:
 	return lerp(a, b, randf())
 
+
 # https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly
 ## Returns a random point in a ring.
 ## The result is uniform.
@@ -40,10 +41,12 @@ static func ring(min_radius: float, max_radius: float, center := Vector2.ZERO) -
 	var r := sqrt(randf_range(min_radius ** 2, max_radius ** 2))
 	return (Vector2.LEFT * r).rotated(randf() * TAU) + center
 
+
 ## Returns a random point in a circle.
 ## The result is uniform.
 static func circle(radius := 1.0, center := Vector2.ZERO) -> Vector2:
 	return (Vector2.LEFT * sqrt(randf()) * radius).rotated(randf() * TAU) + center
+
 
 ## Returns a random point in a triangle.
 ## It's vertex are [param a], [param b] and [param c].
@@ -54,7 +57,8 @@ static func triangle(a: Vector2, b: Vector2, c: Vector2) -> Vector2:
 	if (wb + wc) > 1:
 		wb = 1 - wb
 		wc = 1 - wc
-	return wb * (b - a) +  wc * (c - a) + a
+	return wb * (b - a) + wc * (c - a) + a
+
 
 ## Returns a string containing only characters from [param chars]
 ## [codeblock]
@@ -102,20 +106,25 @@ class ShuffleBag:
 	var _items := []
 	var _left := []
 
+
 	func refill() -> void:
 		_left = _items.duplicate()
 		_left.shuffle()
+
 
 	func next() -> Variant:
 		if _left.is_empty():
 			refill()
 		return _left.pop_back()
 
+
 	func _iter_init(_iter):
 		return not _left.is_empty()
 
+
 	func _iter_next(_iter):
 		return not _left.is_empty()
+
 
 	func _iter_get(_iter):
 		return _left.pop_back()
@@ -140,6 +149,7 @@ class BinarySearchWRS:
 	var _left := 0
 	var _right := 0
 
+
 	func assign(dict: Dictionary) -> void:
 		_items = dict
 		_search_arr = [0]
@@ -147,11 +157,13 @@ class BinarySearchWRS:
 		for key in dict:
 			_search_arr.append(_search_arr[-1] + dict[key])
 
+
 	func pick() -> Variant:
 		_left = 0
 		_right = _search_arr.size()
 		var rand := randf_range(0, _search_arr[-1])
 		return _search(rand)
+
 
 	func _search(rand: float) -> Variant:
 		if abs(_left - _right) <= 1:
@@ -193,8 +205,10 @@ class AResWRS:
 	extends RefCounted
 	var _items: Dictionary
 
+
 	func assign(dict: Dictionary):
 		_items = dict.duplicate()
+
 
 	func pop(count := 1) -> Array:
 		var pool := _calc_eigen_value()
@@ -205,8 +219,9 @@ class AResWRS:
 			_items.erase(key)
 		return result
 
+
 	func _calc_eigen_value() -> Dictionary:
-		var pool := {}
+		var pool := { }
 		for key in _items:
 			var weight: float = _items[key]
 			var eigen := pow(randf(), 1 / weight)
@@ -233,10 +248,12 @@ class AliasWRS:
 	var _small: Array[Array] = []
 	var _large: Array[Array] = []
 
+
 	func assign(dict: Dictionary) -> void:
 		_items = dict
 		_init_queue()
 		_construct_alias_table()
+
 
 	func pick() -> Variant:
 		var area: Array = alias_table.pick_random()
@@ -245,6 +262,7 @@ class AliasWRS:
 			return area[0][0]
 		else:
 			return area[1][0]
+
 
 	func _init_queue():
 		var sum: float = _items.values().reduce(func(a, b): return a + b)
@@ -256,6 +274,7 @@ class AliasWRS:
 				_large.append([key, area])
 			else:
 				_small.append([key, area])
+
 
 	func _construct_alias_table():
 		while _small and _large:
