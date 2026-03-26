915d1cd04f27d542733bfd4f7dbb5d9edd6d9783
Silver1078682 <ShinySmoothSilver@outlook.com>
2026-03-26 21:59:34 +0800

    Feat(CircularQueue): new module

---
+++ addons/gdlib/data_structs/circular_queue.gd
@@ -0,0 +1,110 @@
+class_name CircularQueue
+extends RefCounted
+## A circular queue implementation using an array.
+##
+## Basic usage:
+## [codeblock]
+## var queue := CircularQueue.new(10)
+## queue.push_back(1) # O(1) enqueue
+## var a = queue.pop_front() # O(1) dequeue
+## [/codeblock]
+## You can also pass an initial array to the constructor to initialize the queue from an array
+## [codeblock]
+## var queue := CircularQueue.new(3, [1, 2, 3])
+## var typed_arr : Array[int]
+## var typed_queue := CircularQueue.new(3, typed_arr) # Initialize queue with typed array
+## queue.push_back("string")
+## typed_queue.push_back("string, but in a 'typed queue'") # Raises an error
+## [/codeblock]
+
+## The _capacity of the queue
+var _capacity: int = 0
+var _head: int = 0
+var _size: int = 0
+var _datas: Array
+
+
+## Returns true if the array is empty ([]). See also [method is_full].
+func is_empty():
+	return _size == 0
+
+
+## Returns true if the array is full (size == capacity). also [method is_empty].
+func is_full():
+	return _size == _capacity
+
+
+## Returns the number of elements in the array. Empty arrays ([]) always return 0.
+func size():
+	return _size
+
+
+## Removes and returns the first element of the queue. Returns null if the queue is empty. See also [method push_back].
+func pop_front() -> Variant:
+	if _size == 0:
+		push_error("Can not call pop front on an empty array")
+		return null
+	_head += 1
+	_head = _head % _capacity
+	_size -= 1
+	return _datas[_head - 1]
+
+
+## Appends an element at the end of the queue. See also [method pop_front].
+func push_back(value: Variant) -> void:
+	if _size == _capacity:
+		push_error("Can not call push back on a full array")
+		return
+	var _tail = (_head + _size) % _capacity
+	_datas[_tail] = value
+	if is_same(_datas[_tail], value): # Ensure the value modification succeed
+		_size += 1
+
+
+## Returns the first element of the queue. If the queue is empty, fails and returns null. See also [method back].
+func front() -> Variant:
+	if _size != 0:
+		return _datas[_head]
+	push_error("Can't take value from empty array.")
+	return null
+
+
+## Returns the last element of the queue. If the queue is empty, fails and returns null. See also [method front].
+func back() -> Variant:
+	if _size != 0:
+		return _datas[(_head + _size - 1) % _capacity]
+	push_error("Can't take value from empty array.")
+	return null
+
+
+## Clears the queue.
+func clear() -> void:
+	if _size == 0:
+		return
+	_size = 0
+
+
+## Returns the Variant element at the specified index. Arrays start at index 0.
+## If index is greater or equal to 0, the element is fetched starting from the beginning of the array.
+## If index is a negative value, the element is fetched starting from the end.
+## Accessing an array out-of-bounds will fails.
+func get_at(index: int) -> Variant:
+	if index < -_size or index >= _size:
+		push_error('Out of bound gets index "%d"' % index)
+		return
+	return _datas[(_head + posmod(index, _size)) % _capacity]
+
+
+## Returns the array representation of the circular queue.
+## The array is ordered from the front to the back.
+func to_array() -> Array:
+	return _datas.slice(_head, _head + _size) + _datas.slice(0, max(0, _head + _size - _capacity))
+
+
+func _init(p_capacity: int = 0, typed_arr := []) -> void:
+	if p_capacity:
+		_capacity = p_capacity
+	if typed_arr or typed_arr.is_typed():
+		_datas = typed_arr
+		_size = min(typed_arr.size(), _capacity)
+	_datas.resize(_capacity)
---
+++ tests/data_structs/circular_queue.gd
@@ -0,0 +1,149 @@
+extends GutTest
+
+func before_all():
+	GutTestHelper.coverage(CircularQueue, self)
+
+
+var circular: CircularQueue
+
+
+func before_each() -> void:
+	circular = CircularQueue.new()
+
+
+func test_is_empty() -> void:
+	assert_true(circular.is_empty())
+	assert_true(circular.size() == 0)
+
+
+func test_is_full() -> void:
+	assert_true(circular.is_full())
+	circular = CircularQueue.new(1)
+	assert_false(circular.is_full())
+	circular.push_back(1)
+	assert_true(circular.is_full())
+
+
+func test_front() -> void:
+	assert_null(circular.front())
+	assert_push_error_count(1)
+
+
+func test_back() -> void:
+	assert_null(circular.back())
+	assert_push_error_count(1)
+
+
+func test_one() -> void:
+	circular = CircularQueue.new(10)
+	circular.push_back(1)
+	assert_false(circular.is_empty())
+	assert_eq(circular.front(), 1)
+	assert_eq(circular.back(), 1)
+
+
+func test_size() -> void:
+	assert_eq(circular.size(), 0)
+	circular = CircularQueue.new(10)
+	assert_eq(circular.size(), 0)
+	for i in 10:
+		circular.push_back(i)
+		assert_eq(circular.size(), i + 1)
+
+
+func test_push_back() -> void:
+	circular = CircularQueue.new(10)
+	assert(circular.is_empty())
+	for i in range(10):
+		circular.push_back(i)
+		assert_eq(circular.front(), 0)
+		assert_eq(circular.back(), i)
+		assert_eq(circular.size(), i + 1)
+		assert_push_error_count(0)
+	circular.push_back(10)
+	assert_true(circular.is_full())
+	assert_push_error_count(1)
+
+
+func test_pop_front() -> void:
+	circular = CircularQueue.new(10)
+	assert(circular.is_empty())
+	for i in range(10):
+		circular.push_back(i)
+	for i in range(10):
+		assert_eq(circular.front(), i)
+		assert_eq(circular.back(), 9)
+		assert_eq(circular.size(), 10 - i)
+		assert_eq(circular.pop_front(), i)
+		assert_push_error_count(0)
+	circular.pop_front()
+	assert_push_error_count(1)
+
+
+func test_clear() -> void:
+	circular = CircularQueue.new(10)
+	for i in range(10):
+		circular.push_back(i)
+		assert_eq(circular.front(), 0)
+		assert_eq(circular.back(), i)
+		assert_eq(circular.size(), i + 1)
+	circular.clear()
+	assert_true(circular.is_empty())
+	assert_true(circular.size() == 0)
+	assert_push_error_count(0)
+	assert_null(circular.pop_front())
+	assert_push_error_count(1)
+
+
+func test_get_at() -> void:
+	circular = CircularQueue.new(10)
+	# Test getting elements from the queue
+	circular.push_back(1)
+	circular.push_back(2)
+	circular.push_back(3)
+
+	# Test getting front element
+	assert_eq(circular.get_at(0), 1)
+	# Test getting back element
+	assert_eq(circular.get_at(-1), 3)
+	# Test getting middle element
+	assert_eq(circular.get_at(1), 2)
+
+	# Test out-of-bounds access
+	assert_eq(circular.get_at(-4), null)
+	assert_eq(circular.get_at(3), null)
+	assert_push_error_count(2)
+
+
+func test_typed_queue() -> void:
+	var typed_arr: Array[int] = [1, 2, 3]
+	circular = CircularQueue.new(4, typed_arr)
+	assert_eq(circular.size(), 3)
+	assert_eq(circular.get_at(0), 1)
+	assert_eq(circular.get_at(-1), 3)
+
+	# Test pushing a string to a typed queue
+	circular.push_back("string") # Should fail because the queue
+	assert_engine_error_count(2)
+
+	# Test pushing an int to a typed queue
+	circular.push_back(4)
+	assert_true(circular.is_full())
+	assert_eq(circular.size(), 4)
+	assert_eq(circular.get_at(0), 1)
+	assert_eq(circular.get_at(-1), 4)
+
+
+func test_to_array() -> void:
+	circular = CircularQueue.new(10)
+	assert_true(circular.is_empty())
+	assert_eq(circular.size(), 0)
+	assert_true(circular.to_array().is_empty())
+	for i in 10:
+		circular.push_back(i)
+		assert_eq(circular.to_array(), range(i + 1))
+	assert_true(circular.to_array().size() == 10)
+	for i in 10:
+		circular.pop_front()
+		assert_eq(circular.to_array(), range(i + 1, 10))
+	assert_true(circular.to_array().is_empty())
