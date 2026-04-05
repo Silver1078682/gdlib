1d0b7d3b918dfc8c5ade5f41e6c2671ac4978a77
Silver1078682 <ShinySmoothSilver@outlook.com>
2026-04-05 11:08:19 +0800

    Feat(FileUtil): add open_config_file method

--- addons/gdlib/utils/file.gd
+++ addons/gdlib/utils/file.gd
@@ -28,6 +28,23 @@ static func open_dir(path: String, force := false) -> DirAccess:
 	return dir
 
 
+## Open a ConfigFile, print human-readable error messages on failure.
+## When force set true, create the ConfigFile if the [ConfigFile] at [param path] does not exist.
+static func open_config_file(path: String, force := false) -> ConfigFile:
+	var file: ConfigFile = ConfigFile.new()
+	var error: Error
+	if FileAccess.file_exists(path):
+		error = file.load(path)
+		if error:
+			_print_error(error, OPEN_CONFIG_FILE_FAILURE_MESSAGE, path)
+	elif force:
+		error = file.save(path)
+		if error:
+			_print_error(error, CREATE_CONFIG_FILE_FAILURE_MESSAGE, path)
+
+	return file
+
+
 ## Clear a directory.
 static func clear_dir(dir: DirAccess, recursive := false) -> void:
 	dir.list_dir_begin()
@@ -88,6 +105,8 @@ static func preload_resources(folder_path: String, type_hint: String = "", recur
 const OPEN_FILE_FAILURE_MESSAGE = "opening file at %s failed: "
 const OPEN_DIR_FAILURE_MESSAGE = "opening directory at %s failed: "
 const CREATE_DIR_FAILURE_MESSAGE = "creating directory at %s failed: "
+const OPEN_CONFIG_FILE_FAILURE_MESSAGE = "opening config file at %s failed: "
+const CREATE_CONFIG_FILE_FAILURE_MESSAGE = "creating config file at %s failed: "
 
 
 static func _print_error(error: Error, message: String, path: String) -> void:
